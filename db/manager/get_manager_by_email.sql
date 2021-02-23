41001c8b2f35357d44fd8a1dea1cb0bb7c3c5ef8
Kenyan <gommkm@gmail.com>
2021-02-23 14:33:28 -0700

    changed manager dash to use propertyid when getting workorders

--- db/manager/get_manager_by_email.sql
+++ db/manager/get_manager_by_email.sql
@@ -1,4 +1,4 @@
-select m.id as managerid, m.landlordid, m.firstname, m.lastname, m.password, p.name
+select m.id as managerid, m.propertyid, m.landlordid, m.firstname, m.lastname, m.password, p.name
 from managers m
 join properties p on p.id = m.propertyid
 where m.email = $1
